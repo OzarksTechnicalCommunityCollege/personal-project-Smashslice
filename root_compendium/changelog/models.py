5758ab9080c7458642193a8687549dfdd1f191a9
Paul <76267709+Smashslice@users.noreply.github.com>
2026-01-27 23:27:21 -0600

    Fixed indentation issue in models. Updated templates to include a homepage. Added an entry point in the root urls. Style changes & code organization.

--- root_compendium/changelog/models.py
+++ root_compendium/changelog/models.py
@@ -11,34 +11,43 @@ class PublishedManager(models.Manager):
         )
 
 class Post(models.Model):
+    
+    # Sub classes
+    
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
+        
+    # Properties
+    
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
-    )
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    published = PublishedManager()
+    
     status = models.CharField(
         max_length=2,
         choices=Status,
         default=Status.DRAFT
     )
     
-    objects = models.Manager()
-    published = PublishedManager()
-
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    
+    # Metadata
+    
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
         ]
-    
-def __str__(self):
-    return self.title
+        
+    def __str__(self):
+        return self.title
--- root_compendium/root_compendium/urls.py
+++ root_compendium/root_compendium/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='changelog/home.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('changelog/', include('changelog.urls', namespace='changelog'))
+    path('changelog/', include('changelog.urls',namespace='changelog'))
 ]
