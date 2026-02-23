6c697a0977419700c2ab70f99c617f5f8a30b2cd
Paul <76267709+Smashslice@users.noreply.github.com>
2026-02-22 22:11:35 -0600

    started reset password implementation

--- root_compendium/users/urls.py
+++ root_compendium/users/urls.py
@@ -8,5 +8,27 @@ urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
-    path('edit/', views.edit, name='edit')
+    path('edit/', views.edit, name='edit'),
+    
+    #urls for password changes
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'password-reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path('password-reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
+    
 ]
